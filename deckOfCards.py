03ff3138ced26a5ad984c2d0b721bdc23ec7d73d
Devin Leung <leung.devin@yahoo.com>
2019-10-06 15:51:13 -0700

    implement game flow and breakpoint total

--- deckOfCards.py
+++ deckOfCards.py
@@ -50,6 +50,18 @@ class Card:
         self.suit = suit
         self.face = face
 
+class Discard_Pile:
+
+    def __init__(self):
+        self.cards = []
+
+    def printPile(self):
+        if len(self.cards) > 0: 
+            print("Discard Pile: ", end = '') 
+            for i in range(len(self.cards) ):
+                print(self.cards[-i][0][0] + self.cards[-i][1], end = ' ')
+                if i == len(self.cards)-1:
+                    print('\n')
 
 class Player:
 
@@ -57,6 +69,7 @@ class Player:
         self.name = name
         self.hand = []
         self.handValue = 0
+        self.gameTotal = 0
 
     def draw(self, deck):
         self.hand.append(deck.drawCard())
@@ -68,8 +81,12 @@ class Player:
             print(self.hand[i][0][0] + self.hand[i][1])
         print("Total: " + str(self.handValue))
 
-    def discardAndDraw(self,deck):
+    def discardAndDraw(self,deck,discardPile):
         invalidInput = True
+        discardPile.printPile()
+        self.showHand()
+        drawChoice = 1
+        
         while invalidInput:
             try:
                 cardsToDiscard = input("Which card(s) would you like to drop? ").split(' ')
@@ -78,11 +95,22 @@ class Player:
             except:
                 print("Enter a single number or multiple numbers seperated by a space.")
                 invalidInput = True
-        
+            
+            if len(discardPile.cards) > 0:
+                drawChoice = input("Would you like to draw from the deck(1) or pile(2)? ")
+                if int(drawChoice) == 2 and len(discardPile.cards) > 1:
+                    pileChoice = input("Which card in the pile? ")
+            
         if len(discardList) == 1:
-            self.handValue -= self.hand[(discardList[0]-1)][0][1] 
-            self.hand.pop(discardList[0]-1)
-            self.hand.append(deck.drawCard())
+            tempPile = discardPile.cards
+            self.handValue -= self.hand[(discardList[0])-1][0][1] 
+            discardPile.cards = []
+            discardPile.cards.append(self.hand.pop(discardList[0]-1))
+            print("Discard pile: " +discardPile.cards[0][0][0] + discardPile.cards[0][1])
+            if int(drawChoice) == 1:
+                self.hand.append(deck.drawCard())
+            else:
+                self.hand.append(tempPile[0])
             self.handValue += self.hand[(len(self.hand))-1][0][1]
             return False
         elif len(discardList) > 1:
@@ -91,11 +119,9 @@ class Player:
             faceValue = self.hand[(discardList[0]-1)][0][0]
             suitValue = self.hand[(discardList[0]-1)][1]
             if not all(self.hand[cardPosition-1][0][0] == faceValue 
-                       for cardPosition in discardList
-            ):
+                       for cardPosition in discardList) and len(discardList) > 2:
                 if all(self.hand[cardPosition-1][1] == suitValue 
-                       for cardPosition in discardList
-                ):
+                       for cardPosition in discardList):
                     sortedDiscardFaces = [self.hand[i-1][0][0] for i in discardList]
                     indexesOfFaces = []
                     for subList in deck.cardValues:
@@ -103,19 +129,32 @@ class Player:
                             if face in subList:
                                 indexesOfFaces.append(deck.cardValues.index(subList))
                     indexesOfFaces.sort()
-                
+                    x = 1
                     for n in indexesOfFaces[:len(indexesOfFaces)-1]:
-                        for x in range(1, len(indexesOfFaces)):
-                            if not(int(n + 1) == int(indexesOfFaces[x])):
+                        if not(int(n + 1) == int(indexesOfFaces[x])):
                                 dontDraw = True
+                        x += 1
                 else:
                     dontDraw = True                    
-             
+            elif all(self.hand[cardPosition-1][0][0] == faceValue 
+                       for cardPosition in discardList):
+                dontDraw = False
+            else:
+                dontDraw = True 
             if not dontDraw:            
+                tempPile = discardPile.cards[::]
+                discardPile.cards = []
                 for cardPosition in discardList:
                     self.handValue -= self.hand[cardPosition-1][0][1]
-                    self.hand.pop(cardPosition-1)
-                self.hand.append(deck.drawCard())
+                    discardPile.cards.append(self.hand.pop(cardPosition-1))
+                    
+                discardPile.printPile()
+                if int(drawChoice) == 1:
+                    self.hand.append(deck.drawCard())
+                elif len(tempPile) == 1:
+                    self.hand.append(tempPile[0])
+                else:
+                    self.hand.append(tempPile[int(pileChoice)])
                 self.handValue += self.hand[(len(self.hand))-1][0][1]
                 return False
             else:
--- tonks.py
+++ tonks.py
@@ -2,6 +2,7 @@ import deckOfCards
 
 
 
+
 def compareAllHands():
     smallestHand = 51
     for player in players:
@@ -12,12 +13,32 @@ def compareAllHands():
             winner = player.name
     print(winner + " has a lower hand with a total score of " + str(smallestHand))
 
+def highestGameTotal():
+    highestTotal = 0
+    for player in players:
+        if player.gameTotal > highestTotal:
+            highestTotal = player.gameTotal
+    return highestTotal
+
+def initRound(players,deck):
+    deck.createDeck()
+    deck.shuffle()
+    for player in range(len(players)):
+        players[player].hand = []
+        players[player].handValue = 0
+        for i in range(5):
+            players[player].draw(deck)
+        
+
+
 
 deck = deckOfCards.Deck()
 deck.createDeck()
 deck.shuffle()
 
-playerNames = ['D']
+discardPile = deckOfCards.Discard_Pile()
+
+playerNames = ['D', 'P', 'N']
 players = [deckOfCards.Player(playerName) for playerName in playerNames]
 
 
@@ -25,11 +46,39 @@ for player in players:
     for i in range(5):
         player.draw(deck)
 
-compareAllHands()
 
-while True:
-    if players[0].discardAndDraw(deck) == False:
-        break
 
-players[0].showHand()
+breakpointTotal = int(input("How many total points would you like to play up until: "))
+
+while highestGameTotal() < breakpointTotal:
+    initRound(players, deck)
+    roundOver = False
+    while  not roundOver:
+        print('\n~~~~~~~Start of a new round!~~~~~~~\n')
+        for player in range(len(players)):
+            turnOver = False
+            while not turnOver:
+                print("It is " + players[player].name + "'s turn.")
+                playerAction = input("Call tonks('t') or draw('d') or show hand('s'): ")
+                if playerAction == 't':
+                    compareAllHands()
+                    turnOver = True
+                    roundOver = True
+                    
+                elif playerAction == 'd':
+                    while players[player].discardAndDraw(deck,discardPile) == True:
+                        pass
+                    turnOver = True
+                elif playerAction == 's':
+                    players[player].showHand()
+                else:
+                    print('Enter a valid option')
+                    playerAction = input("Call tonks('t') or draw('d') or show hand('s'): ")
+            if roundOver:
+                for player in players:
+                    player.gameTotal += player.handValue
+                break
+    if highestGameTotal() >= breakpointTotal:
+        print("Game Over")          
+
 
